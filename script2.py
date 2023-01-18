8b539be4445ccc3aa13c3348c9c221b9380ce276
Rajxkaran <rrajekar@flemingcollege.ca>
2023-01-18 15:42:21 -0500

    modified both files in the repo clone
    - deleted a print()  statement from script1.py
    - added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 print("If the implementation is hard to explain, it is a bad idea")
 print("If the implementation is easy to explain, it may be a good idea.")
 print("Namespaces are one honking great idea -- let's do more of those!")
+print("thanks")
