582bd0ccabf1e24d489c847f0ad4fac7c91bdf4e
Rajxkaran <rrajekar@flemingcollege.ca>
2023-01-18 14:40:05 -0500

    Revert "modified both script files"
    This reverts commit a2b82f82102300dc74883096c99ebb473beb53bf.

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
-# This is good programing advice.
+# This is good programing advice 
 print("If the implementation is hard to explain, it is a bad idea")
 print("If the implementation is easy to explain, it may be a good idea.")
 print("Namespaces are one honking great idea -- let's do more of those!")
