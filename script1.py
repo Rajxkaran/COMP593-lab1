9c2e2bcb53865101cd6edf79243f0657ac4703a3
Rajxkaran <rrajekar@flemingcollege.ca>
2023-01-18 14:35:53 -0500

    added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,3 +1,3 @@
-# print lines from zen of python 
+# print lines from zen of python.
 print('There should be one-- and preferably only one --obvious way to do it.')
 print("Although that way may not be abovious at first unless you're Dutch.")
